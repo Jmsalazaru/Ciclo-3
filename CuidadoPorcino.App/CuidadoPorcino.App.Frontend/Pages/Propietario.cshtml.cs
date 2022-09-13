8447d09266c016b82b5ea1df3847bb99ed086b29
David Garrido <www.davidalexander@gmail.com>
2022-09-13 06:49:00 -0500

    plantillas full

---
+++ CuidadoPorcino.App/CuidadoPorcino.App.Frontend/Pages/Propietario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CuidadoPorcino.App.Frontend.Pages
+{
+    public class PropietarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CuidadoPorcino.App/CuidadoPorcino.App.Frontend/Pages/Veterinario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CuidadoPorcino.App.Frontend.Pages
+{
+    public class VeterinarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
